d57fb6614c14395a37bf57a9d31171a8f999f082
Artur Fandiienko <artur.fandiienko@symphony-solutions.com>
2025-10-30 11:06:50 +0200

    #2

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,25 @@
 const CACHE_NAME = 'bh-casino-bot-v1';
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(self.skipWaiting());
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/chat-bot.js",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+      caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+          .then(() => self.skipWaiting())
+  );
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches
-      .keys()
-      .then((cacheNames) =>
-        Promise.all(
-          cacheNames
-            .filter((cacheName) => cacheName !== CACHE_NAME)
-            .map((cacheName) => caches.delete(cacheName)),
-        ),
-      )
-      .then(() => self.clients.claim()),
+      caches.keys().then(keys =>
+          Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+      ).then(() => self.clients.claim())
   );
 });
 
